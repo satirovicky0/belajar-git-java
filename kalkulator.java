6d6db81b7cff9421aa991f22914bc38698c4b589
satriovicky0 <satriovicky0@gmail.com>
2026-06-15 08:42:16 +0700

    versi awal kalkulator

---
+++ kalkulator.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class kalkulator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== Kalkulator Sederhana ===");
+
+        System.out.print("Masukkan angka pertama: ");
+        double angka1 = input.nextDouble();
+
+        System.out.print("Masukkan operator (+, -, *, /): ");
+        char operator = input.next().charAt(0);
+
+        System.out.print("Masukkan angka kedua: ");
+        double angka2 = input.nextDouble();
+
+        double hasil;
+
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+
+            case '-':
+                hasil = angka1 - angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+
+            case '*':
+                hasil = angka1 * angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+
+            case '/':
+                if (angka2 != 0) {
+                    hasil = angka1 / angka2;
+                    System.out.println("Hasil: " + hasil);
+                } else {
+                    System.out.println("Error: Tidak bisa dibagi dengan nol.");
+                }
+                break;
+
+            default:
+                System.out.println("Operator tidak valid.");
+        }
+
+        input.close();
+    }
+}
